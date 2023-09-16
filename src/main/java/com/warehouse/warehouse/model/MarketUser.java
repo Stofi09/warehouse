a57e3c881a726dcef9295550511206469931faf7
Stofi09 <kristof.solak@gmail.com>
2023-09-16 11:12:21 +0200

    Address entity created

--- src/main/java/com/warehouse/warehouse/model/MarketUser.java
+++ src/main/java/com/warehouse/warehouse/model/MarketUser.java
@@ -11,6 +11,39 @@ public class MarketUser {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "password", nullable = false, length = 1000)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 350)
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
