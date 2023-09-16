8c76503f67b39507951f09bbc259c262a9820dbd
Stofi09 <kristof.solak@gmail.com>
2023-09-16 17:59:46 +0200

    More validation added

--- src/main/java/com/warehouse/warehouse/model/dto/UserDTO.java
+++ src/main/java/com/warehouse/warehouse/model/dto/UserDTO.java
@@ -1,11 +1,18 @@
 package com.warehouse.warehouse.model.dto;
 
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.*;
 
 public class UserDTO {
+    @NotNull
+    @NotBlank
     private String name;
+    @NotNull
+    @NotBlank
     @Email(message = "Invalid email format")
     private String email;
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$")
     private String password;
     public UserDTO(){}
     public UserDTO(String name, String email, String password) {
