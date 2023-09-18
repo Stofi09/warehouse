26e7259e4c85721853567e7747329b11f2754785
Stofi09 <kristof.solak@gmail.com>
2023-09-18 12:00:53 +0200

    Method added

--- src/main/java/com/warehouse/warehouse/repository/UserOrderRepository.java
+++ src/main/java/com/warehouse/warehouse/repository/UserOrderRepository.java
@@ -1,10 +1,14 @@
 package com.warehouse.warehouse.repository;
 
+import com.warehouse.warehouse.model.MarketUser;
 import com.warehouse.warehouse.model.UserOrder;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserOrderRepository extends ListCrudRepository<UserOrder,Long> {
+    List<UserOrder> findByUser(MarketUser user);
 
 }
