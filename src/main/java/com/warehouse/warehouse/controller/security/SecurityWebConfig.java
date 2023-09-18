a6771076a785a3a287a8fe2a62f12313ec348ecb
Stofi09 <kristof.solak@gmail.com>
2023-09-18 16:08:56 +0200

    Code formatted

--- src/main/java/com/warehouse/warehouse/controller/security/SecurityWebConfig.java
+++ src/main/java/com/warehouse/warehouse/controller/security/SecurityWebConfig.java
@@ -4,21 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -31,20 +21,6 @@ public class SecurityWebConfig {
         this.jwtRequestFilter = jwtRequestFilter;
     }
 
-  /*  @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().disable();
-        // We need to make sure our authentication filter is run before the http request filter is run.
-        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
-        http.authorizeHttpRequests()
-                // Specific exclusions or rules.
-                .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
-                // Everything else should be authenticated.
-                .anyRequest().authenticated();
-        return http.build();
-    }
-
-   */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
